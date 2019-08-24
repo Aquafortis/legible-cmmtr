c3380566d1eaadfe942d47f45c22a2465baa7dfc
Aquafortis <support@320up.com>
2019-08-24 17:02:21 +1000

    Tagging version 1.3

--- readcmmtr.js
+++ readcmmtr.js
@@ -2,14 +2,3 @@
  * Copyright (c) 2019 Aquafortis
  * Licensed under MPL-2.0 (https://github.com/Aquafortis/legible-cmmtr)
  */
-var website = location.href;
-if (website.substring(1).match("cmmtr.com")) {
-    window.addEventListener("load", () => {
-        var head = document.getElementsByTagName("head")[0];
-        var style = document.createElement("link");
-        style.rel = "stylesheet";
-        style.type = "text/css";
-        style.href = browser.runtime.getURL("readcmmtr.css");
-        head.appendChild(style);
-    });
-}
